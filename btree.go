82e38d0e974f830a46482f2251acc1428011ea81
amyjzhu <ajyzhu@gmail.com>
2018-02-25 19:40:58 -0800

    query, delete, sum

--- btree.go
+++ btree.go
@@ -9,6 +9,9 @@ type BTree struct {
 }
 
 func (tree *BTree) print() {
+	if (tree == nil) {
+		return
+	}
 	fmt.Println(tree.value)
 	// ugh, I'm lazy
 	tree.left.print()
@@ -31,13 +34,13 @@ func insert(tree *BTree, target int) {
 	}
 
 	if (tree.value > target) {
-		if tree.left != nil {
+		if tree.left == nil {
 			tree.left = &node
 		} else {
 			insert(tree.left, target)
 		}
 	} else if (tree.value < target) {
-		if tree.right != nil {
+		if tree.right == nil {
 			tree.right = &node
 		} else {
 			insert(tree.right, target)
@@ -45,3 +48,73 @@ func insert(tree *BTree, target int) {
 	}
 }
 
+func query(tree *BTree, target int) bool {
+
+	if (tree == nil) {
+		return false;
+	}
+
+	if (tree.value == target) {
+		return true;
+	} else if (tree.value > target) {
+		return query(tree.left, target)
+	} else if (tree.value < target) {
+		return query(tree.right, target)
+	}
+
+	return false;
+}
+
+func deleteTree(tree *BTree, target int) {
+	_deleteTree(tree, target, nil, false)
+}
+// can I just assign the new tree
+// this is so ugly
+func _deleteTree(tree *BTree, target int, parent *BTree, right bool) {
+	// first find element
+	if (target > tree.value) {
+		_deleteTree(tree.right, target, tree, true)
+	}
+
+	if (target < tree.value) {
+		_deleteTree(tree.left, target, tree, false)
+	}
+
+	if (target == tree.value) {
+		// is it root or leaf?
+		if (tree.left == nil && tree.right == nil) {
+			if (right) {
+				parent.right = nil
+			} else {
+				parent.left = nil // cannot use "nil" in BTree assignment...
+				// left or right?
+			}
+		} else if (tree.left == nil) {
+			// !!! need to use pointers here to assign correctly, looks like
+			// I guess assigning the pointer to nil doesn't do anything since it's an argument
+			*tree = *tree.right // possibly nil
+		} else if (tree.right == nil) {
+			*tree = *tree.left
+		} else {
+			reassign := find_leftmost(tree.right)
+			tree.value = reassign
+			_deleteTree(tree.right, reassign, tree, true)
+		}
+	}
+}
+
+func find_leftmost(tree *BTree) int {
+	if tree == nil {
+		return -1
+	}
+
+	var leftmost_so_far int;
+	var leftTree = tree.left
+
+	for leftTree != nil {
+		leftmost_so_far = leftTree.value
+		leftTree = leftTree.left
+	}
+
+	return leftmost_so_far
+}
--- main.go
+++ main.go
@@ -9,14 +9,73 @@ import (
 
 // binsearch, qsort, and btree [insert,querey,delete]
 func main() {
-	sample_array := []int{0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11} // 11 items, missing 8
 
-	run_binsearch(binsearch_inplace, sample_array)
-	run_binsearch(binsearch, sample_array)
+	divisorGreater(2)
+	divisorGreater(1)
+	divisorGreater(8128)
+	divisorGreater(0)
+
+	/*
+	run_btree()
+
+	run_binsearch(binsearch_inplace)
+	run_binsearch(binsearch)
 
 	run_qsort()
+	*/
 }
 
+func run_btree() {
+	tree := BTree{5, &BTree{2, &BTree{1, nil, nil}, &BTree{4, nil, nil}},
+		&BTree{7, &BTree{6, nil, nil}, &BTree{9, &BTree{8, nil, nil}, nil}}}
+
+	run_btree_query(tree)
+
+	run_btree_insert(tree)
+
+	run_btree_delete(tree)
+
+}
+
+func run_btree_delete(tree BTree) {
+	fmt.Println("---------------------------------------")
+	fmt.Println("Starting BTree delete. Deleting 8")
+
+	deleteTree(&tree, 8);
+
+	tree.print()
+
+	fmt.Printf("Does 8 exist now? %b. Should be false\n", query(&tree, 8))
+
+	fmt.Println("Now deleting 2")
+
+	deleteTree(&tree, 2);
+
+	tree.print()
+
+}
+
+func run_btree_query(tree BTree) {
+	fmt.Println("-----------------------------")
+	fmt.Println("Starting BTree query. 10 should not exist")
+	fmt.Println(query(&tree, 10));
+
+	fmt.Println("Starting BTree query. 4 should exist")
+	fmt.Println(query(&tree, 4));
+}
+
+func run_btree_insert(tree BTree) {
+	fmt.Println("-----------------------------")
+	fmt.Println("Now executing btree insert")
+
+	fmt.Println("Old tree")
+	tree.print()
+
+	insert(&tree, 3);
+
+	fmt.Println("New tree")
+	tree.print()
+}
 
 func run_qsort() {
 	sample_array := []int{6, 10, 11, 16, 2, 4, 8, 32, 1, 0, 3, 7}
@@ -29,7 +88,9 @@ func run_qsort() {
 
 }
 
-func run_binsearch(search BinSearch, array []int) {
+func run_binsearch(search BinSearch) {
+	array := []int{0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11} // 11 items, missing 8
+
 	fmt.Printf("Now executing: %s\n", getFunctionName(search))
 	fmt.Printf("%v\n", array)
 	fmt.Println("Searching for 3:")
